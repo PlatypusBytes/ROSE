8be9999918d50a013f9c0e7e21fe598d90dd7bac
A_Noordam <aron.noordam@deltares.nl>
2020-06-05 18:56:10 +0200

    added stiffness of rail pads to the stiffness matrix

--- src/tests/test_train_model.py
+++ src/tests/test_train_model.py
@@ -4,18 +4,29 @@ from src.train_model.track import *
 
 class TestTrainModel:
 
-    def test_mass_matrix_rail(self, set_up_rail):
+    def test_mass_matrix_rail(self, set_up_rail, set_up_sleeper, set_up_rail_pad):
 
-        utrack = UTrack(10)
-
+        utrack = UTrack(3)
 
         utrack.rail = set_up_rail
+        utrack.sleeper = set_up_sleeper
+        utrack.rail_pads = set_up_rail_pad
 
-        utrack.rail.calculate_mass_matrix()
 
-        test = utrack.rail.mass_matrix
+        utrack.calculate_n_dofs()
+        utrack.calculate_mass_matrix()
 
-        # utrack.rail.calculate_length_rail()
+        utrack.rail.calculate_timoshenko_factor()
+        utrack.rail.set_aux_stiffness_matrix()
+
+        utrack.set_global_stiffness_matrix()
+
+        import matplotlib.pyplot as plt
+
+        plt.spy(utrack.global_stiffness_matrix)
+        plt.show()
+        test = utrack.mass_matrix_track
+
 
 
         print('test')
@@ -41,12 +52,12 @@ def set_up_section():
     section.area = 69.682e-4
     section.sec_moment_of_inertia = 2337.9e-8
     section.shear_factor = 0
-    section.n_rail_per_sleeper = 3
+    section.n_rail_per_sleeper = 2
     return section
 
 @pytest.fixture
 def set_up_rail(set_up_material, set_up_section):
-    rail = Rail(10)
+    rail = Rail(3)
     rail.section = set_up_section
     rail.material = set_up_material
 
@@ -55,6 +66,25 @@ def set_up_rail(set_up_material, set_up_section):
     rail.calculate_n_dof()
     return rail
     # rail.section = request.
+
+@pytest.fixture
+def set_up_sleeper():
+    sleeper = Sleeper()
+    sleeper.mass = 162.5
+    sleeper.distance_between_sleepers = 0.6
+    sleeper.damping_ratio = 0.04
+    sleeper.radial_frequency_one = 2
+    sleeper.radial_frequency_two = 500
+    return sleeper
+
+@pytest.fixture
+def set_up_rail_pad():
+    rail_pad = RailPad()
+    rail_pad.mass = 5
+    rail_pad.stiffness = 145e6
+    rail_pad.damping = 12e3
+    return  rail_pad
+
 
 
 # @pytest.fixture()
