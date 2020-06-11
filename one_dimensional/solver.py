7f59d917ef8a5052141a2c6cc877e0b2fb5746d1
Bruno Zuada Coelho <brunozuada@gmail.com>
2020-06-11 11:17:01 +0200

    modified the solver for incremental formulation

--- one_dimensional/solver.py
+++ one_dimensional/solver.py
@@ -5,7 +5,7 @@ import pickle
 from tqdm import tqdm
 
 
-def const(beta, gamma, h):
+def integration_constants(beta, gamma, time_step):
     r"""
     Constants for the Newmark-Beta solver.
 
@@ -15,8 +15,8 @@ def const(beta, gamma, h):
     :param gamma: Parameter :math:`\gamma` that weights the contribution of the initial and final acceleration to the
         change of velocity.
     :type gamma: float
-    :param h: Time step.
-    :type h: float
+    :param time_step: Time step.
+    :type time_step: float
 
     :return a1: Parameter :math:`\alpha_1`.
     :return a2: Parameter :math:`\alpha_2`.
@@ -24,17 +24,14 @@ def const(beta, gamma, h):
     :return a4: Parameter :math:`\alpha_4`.
     :return a5: Parameter :math:`\alpha_5`.
     :return a6: Parameter :math:`\alpha_6`.
-
-    :raises ValueError:
-    :raises TypeError:
     """
 
-    a1 = 1 / (beta * h ** 2)
-    a2 = 1 / (beta * h)
+    a1 = 1 / (beta * time_step ** 2)
+    a2 = 1 / (beta * time_step)
     a3 = 1 / (2 * beta) - 1
-    a4 = gamma / (beta * h)
+    a4 = gamma / (beta * time_step)
     a5 = (gamma / beta) - 1
-    a6 = (gamma / beta - 2) * h / 2
+    a6 = (gamma / beta - 2) * time_step / 2
 
     return a1, a2, a3, a4, a5, a6
 
@@ -86,18 +83,33 @@ class Solver:
 
         return
 
-    def newmark(self, settings, M, C, K, F, t_step, t_end, t_start=0, alpha=0):
+    def newmark(self, settings, M, C, K, F, t_step, t_end, t_start=0):
+        """
+        Newmark integration scheme.
+        Incremental formulation.
 
-        # constants for the Newmark
-        a1, a2, a3, a4, a5, a6 = const(settings["beta"], settings["gamma"], t_step)
+        :param settings: dictionary with the integration settings
+        :param M: Mass matrix
+        :param C: Damping matrix
+        :param K: Stiffness matrix
+        :param F: External force matrix
+        :param t_step: time step for the analysis
+        :param t_end: total time for the analysis
+        :param t_start: start time for the analysis (optional, default is zero)
+        :return:
+        """
 
-        # initial force conditions
-        F_ini = F[:, 0]
+        # constants for the Newmark integration
+        beta = settings["beta"]
+        gamma = settings["gamma"]
+
+        # initial force conditions: for computation of initial acceleration
+        d_force = F[:, 0]
 
         # initial conditions u, v, a
-        u = self.u0
+        u = du = self.u0
         v = self.v0
-        a = init(M, C, K, F_ini, u, v)
+        a = init(M, C, K, d_force, u, v)
         # add to results initial conditions
         self.u.append(u)
         self.v.append(v)
@@ -107,10 +119,10 @@ class Solver:
         self.time = np.linspace(t_start, t_end, int(np.ceil((t_end - t_start) / t_step)))
 
         # combined stiffness matrix
-        K_till = K.dot(1 + alpha) + C.dot(a4).dot(1 + alpha) + M.dot(a1)
+        K_till = K + C.dot(gamma / (beta * t_step)) + M.dot(1 / (beta * t_step ** 2))
 
         # define progress bar
-        pbar = tqdm(total=len(self.time), unit_scale=True, unit_divisor=1000, unit="steps")
+        pbar = tqdm(total=int(len(self.time) - 1), unit_scale=True, unit_divisor=1000, unit="steps")
 
         # iterate for each time step
         for t in range(1, len(self.time)):
@@ -118,33 +130,35 @@ class Solver:
             pbar.update(1)
 
             # updated mass
-            m_part = u.dot(a1) + v.dot(a2) + a.dot(a3)
+            m_part = v.dot(1 / (beta * t_step)) + a.dot(1 / (2 * beta))
             m_part = M.dot(m_part)
             # updated damping
-            c_part = u.dot(a4) + v.dot(a5) + a.dot(a6)
-            c_part = C.dot(c_part).dot(1 + alpha)
+            c_part = v.dot(gamma / beta) + a.dot(t_step * (gamma / (2 * beta) - 1))
+            c_part = C.dot(c_part)
+
+            # update external force
+            d_force = F[:, t] - F[:, t - 1]
 
             # external force
-            force = F[:, t]
-            force_ext = force + m_part + c_part
+            force_ext = d_force + m_part + c_part
 
             # solve
-            uu = solve(K_till, force_ext)
+            du = solve(K_till, force_ext)
 
             # velocity calculated through Newmark relation
-            vv = (uu - u).dot(a4) - v.dot(a5) - a.dot(a6)
+            dv = du.dot(gamma / (beta * t_step)) - v.dot(gamma / beta) + a.dot(t_step * (1 - gamma / (2 * beta)))
             # acceleration calculated through Newmark relation
-            aa = (uu - u).dot(a1) - v.dot(a2) - a.dot(a3)
-
-            # add to results
-            self.u.append(uu)
-            self.v.append(vv)
-            self.a.append(aa)
+            da = du.dot(1 / (beta * t_step ** 2)) - v.dot(1 / (beta * t_step)) - a.dot(1 / (2 * beta))
 
             # update variables
-            u = uu
-            a = aa
-            v = vv
+            u = np.array(u + du)
+            v = np.array(v + dv)
+            a = np.array(a + da)
+
+            # add to results
+            self.u.append(u)
+            self.v.append(v)
+            self.a.append(a)
 
         # convert to numpy arrays
         self.u = np.array(self.u)
@@ -156,11 +170,24 @@ class Solver:
         return
 
     def static(self, K, F, t_step, t_end, t_start=0):
+        """
+        Static integration scheme.
+        Incremental formulation.
+
+        :param K: Stiffness matrix
+        :param F: External force matrix
+        :param t_step: time step for the analysis
+        :param t_end: total time for the analysis
+        :param t_start: start time for the analysis (optional, default is zero)
+        :return:
+        """
 
         # initial conditions u
         u = self.u0
         # add to results initial conditions
         self.u.append(u)
+        # initial differential force
+        d_force = F[:, 0]
 
         # time
         self.time = np.linspace(t_start, t_end, int(np.ceil((t_end - t_start) / t_step)))
@@ -168,18 +195,21 @@ class Solver:
         # define progress bar
         pbar = tqdm(total=len(self.time), unit_scale=True, unit_divisor=1000, unit="steps")
 
-        for t in range(len(self.time)):
+        for t in range(1, len(self.time)):
             # update progress bar
             pbar.update(1)
 
-            # external force
-            force = F[:, t]
-
             # solve
-            uu = solve(K, force)
+            uu = solve(K, d_force)
+
+            # update displacement
+            u = np.array(u + uu)
+
+            # update external force
+            d_force = F[:, t] - F[:, t - 1]
 
             # add to results
-            self.u.append(uu)
+            self.u.append(np.array(u))
 
         # convert to numpy arrays
         self.u = np.array(self.u)
@@ -189,6 +219,9 @@ class Solver:
         return
 
     def save_data(self):
+        """
+        Saves the data into a binary pickle file
+        """
 
         # construct dic structure
         data = {"displacement": self.u,
@@ -199,4 +232,5 @@ class Solver:
         # dump data
         with open(os.path.join(self.output_folder, "data.pickle"), "wb") as f:
             pickle.dump(data, f)
+
         return
