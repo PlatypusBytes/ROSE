7ae7c1a872051bf8f3370438ba3d266e52bf93c6
A_Noordam <aron.noordam@deltares.nl>
2020-08-14 12:49:51 +0200

    added test to check moving point load

--- src/tests/test_boundary_conditions.py
+++ src/tests/test_boundary_conditions.py
@@ -11,14 +11,26 @@ class TestBoundaryConditions:
 
         # set geometry
         nodes_track = [Node(0.0, 0.0, 0.0), Node(1.0, 0.0, 0.0), Node(2.0, 0.0, 0.0)]
-        elements_track = [Element([nodes_track[0], nodes_track[1]]), Element([nodes_track[1], nodes_track[2]])]
+        elements_track = [
+            Element([nodes_track[0], nodes_track[1]]),
+            Element([nodes_track[1], nodes_track[2]]),
+        ]
 
         # set time
         time = np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
 
         # set coordinate of moving load per timestep
-        moving_coords = np.array([Node(0.0, 0.0, 0.0), Node(0.25, 0.0, 0.0), Node(0.5, 0.0, 0.0),
-                                  Node(0.75, 0.0, 0.0), Node(1, 0.0, 0.0), Node(1.25, 0.0, 0.0), Node(1.5, 0.0, 0.0)])
+        moving_coords = np.array(
+            [
+                Node(0.0, 0.0, 0.0),
+                Node(0.25, 0.0, 0.0),
+                Node(0.5, 0.0, 0.0),
+                Node(0.75, 0.0, 0.0),
+                Node(1, 0.0, 0.0),
+                Node(1.25, 0.0, 0.0),
+                Node(1.5, 0.0, 0.0),
+            ]
+        )
 
         # set moving load
         force = LoadCondition(y_disp_dof=True)
@@ -26,16 +38,20 @@ class TestBoundaryConditions:
         force.elements = elements_track
 
         force.y_force = sparse.lil_matrix((len(nodes_track), len(time)))
-        force.set_moving_point_load(moving_coords, time, y_force=np.array([1, 1, 1, 1, 1, 1, 1]))
+        force.set_moving_point_load(
+            moving_coords, time, y_force=np.array([1, 1, 1, 1, 1, 1, 1])
+        )
 
         # set expected values
-        expected_y_force_matrix = [[1, 0.75, 0.5, 0.25, 0.0, 0.0, 0.0],
-                                   [0.0, 0.25, 0.5, 0.75, 1.0, 0.75, 0.5],
-                                   [0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.5]]
+        expected_y_force_matrix = [
+            [1, 0.75, 0.5, 0.25, 0.0, 0.0, 0.0],
+            [0.0, 0.25, 0.5, 0.75, 1.0, 0.75, 0.5],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.5],
+        ]
 
         # assert each value in force matrix
         for i in range(len(expected_y_force_matrix)):
             for j in range(len(expected_y_force_matrix[i])):
-                assert force.y_force[i, j] == pytest.approx(expected_y_force_matrix[i][j])
-
-
+                assert force.y_force[i, j] == pytest.approx(
+                    expected_y_force_matrix[i][j]
+                )
