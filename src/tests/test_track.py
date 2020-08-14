3b5525595036f14e46e734bb28d0626a85ecd673
A_Noordam <aron.noordam@deltares.nl>
2020-08-14 12:53:10 +0200

    reformat

--- src/tests/test_track.py
+++ src/tests/test_track.py
@@ -10,7 +10,9 @@ from one_dimensional.solver import Solver
 
 
 class TestTrack:
-    def test_euler_stiffness_matrix_track(self, set_up_euler_rail, expected_euler_rail_stiffness_matrix):
+    def test_euler_stiffness_matrix_track(
+        self, set_up_euler_rail, expected_euler_rail_stiffness_matrix
+    ):
         rail = set_up_euler_rail
         rail.set_aux_stiffness_matrix()
         rail_euler_stiffness_matrix = rail.aux_stiffness_matrix
@@ -19,9 +21,13 @@ class TestTrack:
 
         for i in range(len(expected_stiffness_matrix)):
             for j in range(len(expected_stiffness_matrix[i])):
-                assert rail_euler_stiffness_matrix[i, j] == pytest.approx(expected_stiffness_matrix[i][j])
+                assert rail_euler_stiffness_matrix[i, j] == pytest.approx(
+                    expected_stiffness_matrix[i][j]
+                )
 
-    def test_euler_mass_matrix_track(self, set_up_euler_rail, expected_euler_rail_mass_matrix):
+    def test_euler_mass_matrix_track(
+        self, set_up_euler_rail, expected_euler_rail_mass_matrix
+    ):
         rail = set_up_euler_rail
         rail.set_aux_mass_matrix()
         rail_euler_mass_matrix = rail.aux_mass_matrix
@@ -30,9 +36,13 @@ class TestTrack:
 
         for i in range(len(expected_mass_matrix)):
             for j in range(len(expected_mass_matrix[i])):
-                assert rail_euler_mass_matrix[i, j] == pytest.approx(expected_mass_matrix[i][j])
+                assert rail_euler_mass_matrix[i, j] == pytest.approx(
+                    expected_mass_matrix[i][j]
+                )
 
-    def test_euler_damping_matrix(self, set_up_euler_rail, expected_euler_rail_damping_matrix):
+    def test_euler_damping_matrix(
+        self, set_up_euler_rail, expected_euler_rail_damping_matrix
+    ):
         rail = set_up_euler_rail
         rail.set_aux_mass_matrix()
         rail.set_aux_stiffness_matrix()
@@ -44,9 +54,13 @@ class TestTrack:
 
         for i in range(len(expected_damping_matrix)):
             for j in range(len(expected_damping_matrix[i])):
-                assert rail_damping_matrix[i, j] == pytest.approx(expected_damping_matrix[i][j])
+                assert rail_damping_matrix[i, j] == pytest.approx(
+                    expected_damping_matrix[i][j]
+                )
 
-    def test_initialize_euler_beam(self, set_up_euler_rail, expected_euler_rail_damping_matrix):
+    def test_initialize_euler_beam(
+        self, set_up_euler_rail, expected_euler_rail_damping_matrix
+    ):
         rail = set_up_euler_rail
         rail.initialize()
 
@@ -56,7 +70,9 @@ class TestTrack:
         assert rail.timoshenko_factor == 0
         for i in range(len(expected_damping_matrix)):
             for j in range(len(expected_damping_matrix[i])):
-                assert rail_damping_matrix[i, j] == pytest.approx(expected_damping_matrix[i][j])
+                assert rail_damping_matrix[i, j] == pytest.approx(
+                    expected_damping_matrix[i][j]
+                )
 
     def test_initialize_timoshenko_beam(self, set_up_euler_rail):
         rail = set_up_euler_rail
@@ -71,7 +87,7 @@ class TestTrack:
 def set_up_material():
     # Steel
     material = Material()
-    material.youngs_modulus = 200E9  # Pa
+    material.youngs_modulus = 200e9  # Pa
     material.poisson_ratio = 0.0
     material.density = 8000
     return material
@@ -109,34 +125,40 @@ def set_up_euler_rail(set_up_material, set_up_euler_section):
 
 @pytest.fixture
 def expected_euler_rail_stiffness_matrix():
-    expected_stiffness_matrix = [[2e7, 0, 0, -2e7, 0, 0],
-                                 [0, 4.8e4, 2.4e5, 0, -4.8e4, 2.4e5],
-                                 [0, 2.4e5, 1.6e6, 0, -2.4e5, 8e5],
-                                 [-2e7, 0, 0, 2e7, 0, 0],
-                                 [0, -4.8e4, -2.4e5, 0, 4.8e4, -2.4e5],
-                                 [0, 2.4e5, 8e5, 0, -2.4e5, 1.6e6]]
+    expected_stiffness_matrix = [
+        [2e7, 0, 0, -2e7, 0, 0],
+        [0, 4.8e4, 2.4e5, 0, -4.8e4, 2.4e5],
+        [0, 2.4e5, 1.6e6, 0, -2.4e5, 8e5],
+        [-2e7, 0, 0, 2e7, 0, 0],
+        [0, -4.8e4, -2.4e5, 0, 4.8e4, -2.4e5],
+        [0, 2.4e5, 8e5, 0, -2.4e5, 1.6e6],
+    ]
     return expected_stiffness_matrix
 
 
 @pytest.fixture
 def expected_euler_rail_mass_matrix():
-    expected_mass_matrix = [[26.66666667, 0, 0, 13.33333333, 0, 0],
-                            [0, 29.73348571, 41.9207619, 0, 10.26651429, -24.74590476],
-                            [0, 41.9207619, 76.40380952, 0, 24.74590476, -57.19619048],
-                            [13.33333333, 0, 0, 26.66666667, 0, 0],
-                            [0, 10.26651429, 24.74590476, 0, 29.73348571, -41.9207619],
-                            [0, -24.74590476, -57.19619048, 0, -41.9207619, 76.40380952]]
+    expected_mass_matrix = [
+        [26.66666667, 0, 0, 13.33333333, 0, 0],
+        [0, 29.73348571, 41.9207619, 0, 10.26651429, -24.74590476],
+        [0, 41.9207619, 76.40380952, 0, 24.74590476, -57.19619048],
+        [13.33333333, 0, 0, 26.66666667, 0, 0],
+        [0, 10.26651429, 24.74590476, 0, 29.73348571, -41.9207619],
+        [0, -24.74590476, -57.19619048, 0, -41.9207619, 76.40380952],
+    ]
     return expected_mass_matrix
 
 
 @pytest.fixture
 def expected_euler_rail_damping_matrix():
-    expected_damping_matrix = [[4005.33333333, 0.00000000, 0.00000000, -3997.33333333, 0.00000000, 0.00000000],
-                               [0.00000000, 15.54669714, 56.38415238, 0.00000000, -7.54669714, 43.05081905],
-                               [0.00000000, 56.38415238, 335.28076190, 0.00000000, -43.05081905, 148.56076190],
-                               [-3997.33333333, 0.00000000, 0.00000000, 4005.33333333, 0.00000000, 0.00000000],
-                               [0.00000000, -7.54669714, -43.05081905, 0.00000000, 15.54669714, -56.38415238],
-                               [0.00000000, 43.05081905, 148.56076190, 0.00000000, - 56.38415238, 335.28076190]]
+    expected_damping_matrix = [
+        [4005.33333333, 0.00000000, 0.00000000, -3997.33333333, 0.00000000, 0.00000000],
+        [0.00000000, 15.54669714, 56.38415238, 0.00000000, -7.54669714, 43.05081905],
+        [0.00000000, 56.38415238, 335.28076190, 0.00000000, -43.05081905, 148.56076190],
+        [-3997.33333333, 0.00000000, 0.00000000, 4005.33333333, 0.00000000, 0.00000000],
+        [0.00000000, -7.54669714, -43.05081905, 0.00000000, 15.54669714, -56.38415238],
+        [0.00000000, 43.05081905, 148.56076190, 0.00000000, -56.38415238, 335.28076190],
+    ]
     return expected_damping_matrix
 
 
