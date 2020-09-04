b047e0b44a9135ebc6fbc4800be1f701f1927bdd
A_Noordam <aron.noordam@deltares.nl>
2020-09-04 09:50:25 +0200

    added function to create animation

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
 
 
 def plot_2d_geometry(elements):
@@ -11,4 +13,24 @@ def plot_2d_geometry(elements):
         plt.plot(x_coords, y_coords, marker="o", markerfacecolor="blue", color="black")
 
     return fig
-    # plt.show()
+
+def create_animation(filename,x_data, y_data: np.array, format='html'):
+    """
+    Creates animation
+    :param filename:
+    :param x_data: 
+    :param y_data:
+    :return:
+    """
+    fig = plt.figure()
+
+    ims = []
+    ax = fig.gca()
+
+    for i in range(len(y_data[0])):
+        ims.append((ax.plot(x_data, list(y_data[:, i]), color='black')[0],))
+
+    writer = animation.writers[format](fps=60)
+    im_ani = animation.ArtistAnimation(fig, ims,
+                                       blit=True)
+    im_ani.save(filename, writer=writer)
