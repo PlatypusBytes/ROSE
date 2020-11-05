f79d533b57209e2f9c1e014eefe904949e290226
Bruno Zuada Coelho <brunozuada@gmail.com>
2020-11-05 17:01:17 +0100

    #9 added damping to SOS
    Former-commit-id: c8b656b86a75bdc7eae3002e102187fc0a282a19

--- rose/run_SOS.py
+++ rose/run_SOS.py
@@ -1,7 +1,7 @@
 from rose.SOS import SoS
 
 sos = SoS.ReadSosScenarios(r"N:\Projects\11203500\11203592\B. Measurements and calculations\TKI Data\Output\soilprofiles.csv",
-                           r"N:\Projects\11203500\11203592\B. Measurements and calculations\TKI Data\Properties\20201029_Prorail_parameters_SOS.csv",
+                           r"N:\Projects\11203500\11203592\B. Measurements and calculations\TKI Data\Properties\20201102_Prorail_parameters_SOS.csv",
                            r"N:\Projects\11203500\11203592\B. Measurements and calculations\TKI Data\Output\segments.csv",
                            r"N:\Projects\11204500\11204953\B. Measurements and calculations\SOS\TKI Data\Segmenten\Segments_TKI_v2.shp")
 sos.create_segments()
