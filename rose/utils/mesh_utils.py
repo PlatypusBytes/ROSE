corrected calculation for cumulative distance between nodes
Former-commit-id: 7ff76f3ddb0c66d49d93efd8f66170ed94a5e802

rose/utils/mesh_utils.py
@@ -124,7 +124,8 @@ def add_moving_point_load_to_track(
             start_coords, np.array(force.elements[element_idx].nodes[0].coordinates)
         )
     # ???
-    cum_distances_force += force.nodes.index(force.elements[element_idx].nodes[0])
+    cum_distances_force += cum_distances_nodes[force.nodes.index(force.elements[element_idx].nodes[0])]
+    # cum_distances_force += force.elements[element_idx].nodes[0].coordinates
 
     # cum_distances_force = (
     #         np.append(