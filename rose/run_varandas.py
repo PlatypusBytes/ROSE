476a53a8a42421ff16ea739115410a9f15fdcd36
Bruno Zuada Coelho <brunozuada@gmail.com>
2021-02-04 11:30:04 +0100

    #11 added varandas model
    Former-commit-id: d3f3fa3461977a97002b3992871d72b3217595d2

---
+++ rose/run_varandas.py
@@ -0,0 +1,31 @@
+from cumulative_models import Varandas
+import numpy as np
+import matplotlib.pylab as plt
+
+
+if __name__ == "__main__":
+
+    import pickle
+    with open(r"./tests/test_data/res_KDyn_Segment 1001_scenario 1_damping_70.pickle", "rb") as f:
+        data = pickle.load(f)
+
+    train_info = {"dubbeldekker": {"forces": data['vertical_force_soil'],
+                                   "nb-per-hour": 6,
+                                   "nb-hours": 5,
+                                   "nb-axles": 4},
+                  "sprinter": {"forces": np.array(data['vertical_force_soil']) / 2,
+                               "nb-per-hour": 4,
+                               "nb-hours": 12,
+                               "nb-axles": 4},
+                  }
+
+    sett = Varandas.AccumulationModel()
+    sett.read_traffic(train_info, 20)
+    sett.settlement()
+
+    fig, ax = plt.subplots(1, 1, figsize=(6, 5))
+    ax.plot(sett.cumulative_time, sett.displacement[100, :])
+    ax.grid()
+    ax.set_xlabel("Time [d]")
+    ax.set_ylabel("Vertical displacement [m]")
+    plt.show()
