7f7e42fe9100c5bf117a328beda14b74e93fc151
A_Noordam <aron.noordam@deltares.nl>
2021-05-10 14:26:12 +0200

    cleaned fugro.py
    Former-commit-id: 767ecc8abdc72547a5867c584ed8b8025411b346

--- data_proc/fugro.py
+++ data_proc/fugro.py
@@ -9,26 +9,35 @@ import itertools
 from pathlib import Path
 from datetime import datetime
 import csv
+from typing import List, Dict
 
-def plot_settlement_in_range_vs_date(res, xlim, ylim, fig=None,position = 111):
+def plot_settlement_in_range_vs_date(res: Dict, xlim: List, ylim: List, fig=None,position = 111):
     """
     Plots the track settlement within a segment for each time step
-    :param xlim:
-    :param ylim:
-    :param res:
+
+    :param res: results dictionary
+    :param xlim: x limit
+    :param ylim: y limit
+    :param fig: optional existing figure
+    :param position: position of subplot, default: 111
+
     :return:
     """
     m_to_mm = 1000
 
+    # initialise figure if it is not an input
     if fig is None:
         fig = plt.figure()
     ax = fig.add_subplot(position)
 
+    # interpolate heights and coordinates on first measurement
     dates, coordinate_data, interpolated_heights = interpolate_coordinates(res, xlim, ylim)
 
     if interpolated_heights.size > 0:
+        # calculate settlement
         settlement = np.subtract(interpolated_heights, interpolated_heights[0,:]) * m_to_mm
 
+        # calculate mean heights per date
         mean_heights = []
         for res_at_t in res["data"]:
             coordinates_in_range, heights_in_range = filter_data_within_bounds(xlim, ylim, res_at_t)
@@ -36,11 +45,9 @@ def plot_settlement_in_range_vs_date(res, xlim, ylim, fig=None,position = 111):
             mean_heights.append(mean_height)
 
         mean_heights = np.array(mean_heights)[np.isfinite(mean_heights)]
-
-        # if mean_heights.size >0:
         mean_settlement = (np.array(mean_heights) - mean_heights[0]) * m_to_mm
 
-
+        # plot settlement and mean settlement vs dates
         ax.plot(dates, settlement, 'o', color='blue')
         ax.plot(dates, mean_settlement, 'o', color='orange')
         ax.set_xlabel("Date")
@@ -52,18 +59,21 @@ def plot_settlement_in_range_vs_date(res, xlim, ylim, fig=None,position = 111):
 def plot_average_height_in_range_vs_date(xlim, ylim, res, fig=None,position = 111):
     """
     Plots the average track settlement within a segment for each time step
-    :param xlim:
-    :param ylim:
-    :param res:
-    :return:
+
+    :param xlim: x limit
+    :param ylim: y limit
+    :param res: results dictionary
+    :param fig: optional existing figure
+    :param position: position of subplot, default: 111
     """
     m_to_mm = 1000
 
-    # fig, ax = plt.subplots()
+    # create figure if it does not exist
     if fig is None:
         fig = plt.figure()
     ax = fig.add_subplot(position)
-    # ax = fig.gca()
+
+    # get height data within limits and calculate mean
     heights = []
     for res_at_t in res["data"]:
         coordinates_in_range, heights_in_range = filter_data_within_bounds(xlim, ylim, res_at_t)
@@ -73,9 +83,12 @@ def plot_average_height_in_range_vs_date(xlim, ylim, res, fig=None,position = 11
     dates = res["dates"][np.isfinite(heights)]
     heights = np.array(heights)[np.isfinite(heights)]
 
+
     if heights.size >0:
+        # calculate average settlement
         settlement = (np.array(heights) - heights[0]) * m_to_mm
 
+        # plot average settlement
         ax.plot(dates, settlement)
         ax.set_xlabel("Date")
         ax.set_ylabel("Average settlement [mm]")
@@ -85,25 +98,34 @@ def plot_average_height_in_range_vs_date(xlim, ylim, res, fig=None,position = 11
 def plot_height_vs_coords_in_range(xlim, ylim, res, fig=None, projection="3d"):
     """
     Plots height versus coordinates
-    :param xlim:
-    :param ylim:
-    :param res:
+
+    :param xlim: x limit
+    :param ylim: y limit
+    :param res: results dictionary
+    :param fig: optional existing figure
     :param projection: Projection of the plot '3d', 'x', or 'y'
     :return:
     """
 
     marker = itertools.cycle((',', '+', '.', 'o', '*'))
-    fig = plt.figure()
+
+    # create figure if it does not exist
+    if fig is None:
+        fig = plt.figure()
 
     if projection == "3d":
         ax = plt.axes(projection='3d')
     else:
         ax = plt.axes()
+
+    # loop over each date
     for res_at_t in res["data"]:
+
+        #get data within limits
         coordinates_in_range, heights_in_range = filter_data_within_bounds(xlim, ylim, res_at_t)
         if coordinates_in_range.size and coordinates_in_range.size:
-            # distance = np.append(0,np.cumsum((np.diff(coordinates_in_range[:,0])**2 + np.diff(coordinates_in_range[:,1])**2) **1/2))
 
+            # plot data on a 3d projecten
             if projection == "3d":
                 ax.plot3D(coordinates_in_range[:,0],coordinates_in_range[:,1], heights_in_range, marker=next(marker),markersize=2)
 
@@ -111,11 +133,14 @@ def plot_height_vs_coords_in_range(xlim, ylim, res, fig=None, projection="3d"):
                 ax.set_ylabel("y-coord")
                 ax.set_zlabel("height [m NAP]")
 
+            # plot the data on x projection
             elif projection == "x":
                 ax.plot(coordinates_in_range[:,0], heights_in_range, marker=next(marker),markersize=2)
 
                 ax.set_xlabel("x-coord")
                 ax.set_ylabel("height [m NAP]")
+
+            # plot the data on y projection
             elif projection == "y":
                 ax.plot(coordinates_in_range[:,1], heights_in_range, marker=next(marker),markersize=2)
 
@@ -141,93 +166,84 @@ def filter_data_within_bounds(xlim, ylim, res):
 
     return coordinates_in_range, heights_in_range
 
-
-# def get_data_at_location(file_dir, location="Amsterdam_Utrecht"):
-#     files = list(Path(file_dir).glob(location + "*"))
-#
-#     res = {"location": location,
-#            "dates": [],
-#            "data": []}
-#
-#     for file in files:
-#         date = datetime.strptime(file.stem.split('_')[-1],"%Y%m")
-#         res["dates"].append(date)
-#         res["data"].append(read_krdz_file(file))
-#
-#     res["dates"] = np.array(res["dates"])
-#     res["data"] = np.array(res["data"])
-#
-#     sorted_indices = np.argsort(res["dates"])
-#
-#     res["dates"] = res["dates"][sorted_indices]
-#     res["data"] = res["data"][sorted_indices]
-#
-#     return res
-
-def get_data_at_location(file_dir, location="all", filetype='csv'):
+def get_data_at_location(file_dir, location: str ="all", filetype: str ='csv') -> Dict:
     """
-    :param file_dir:
-    :param location:
+    :param file_dir: directory where all data files are located
+    :param location: location of the data
     :param filetype: 'csv' or 'KRDZ'
     :return:
     """
 
+    # if location is all, get all files in dir which end with the desired extension
+    # else get all files based on the location
     if location == "all":
         files = list(Path(file_dir).glob("*." + filetype))
     else:
         files = list(Path(file_dir).glob(location + "*"))
 
-
+    # initialise results dictionary
     res = {"location": location,
            "dates": [],
            "data": []}
 
+    # add data from all desired files to dictionary
     for file in files:
         date = datetime.strptime(file.stem.split('_')[-1],"%Y%m")
         res["dates"].append(date)
         if filetype == "KRDZ":
-            res["data"].append(read_krdz_file(file))
+            res["data"].append(read_rila_data_from_krdz(file))
         if filetype == "csv":
-            res["data"].append(read_krdz_data_from_csv(file))
+            res["data"].append(read_rila_data_from_csv(file))
 
+    # convert dates and data to nd arrays
     res["dates"] = np.array(res["dates"])
     res["data"] = np.array(res["data"])
 
+    # sort data based on dates
     sorted_indices = np.argsort(res["dates"])
-
     res["dates"] = res["dates"][sorted_indices]
     res["data"] = res["data"][sorted_indices]
 
     return res
 
 
-def write_krdz_coordinates_to_csv(filename, coordinates):
+def write_krdz_coordinates_to_csv(filename: str, coordinates: np.ndarray):
+    """
+    Writes the fugro data coordinates to a csv file
+    :param filename: output filename
+    :param coordinates: coordinates
+    :return:
+    """
 
     with open(Path(filename + '.csv'), 'w') as f:
         for coord in coordinates:
             f.write(f'{coord[0]};{coord[1]}\n')
 
 
-def read_krdz_file(filename):
+def read_rila_data_from_krdz(filename) -> Dict:
     """
-    Reads krdz file, coordinates are arranged from north to south
-    :param filename:
+    Reads rila data from krdz file
+    :param filename: krdz file name
     :return:
     """
+
+    # read the file
     with open(filename, 'r') as f:
         lines = f.readlines()
 
+    # initialise dictionary
     res = {"coordinates": None,
            "height": None}
 
+    # get coordinates and heights from data
     coords = []
     heights = []
-
     for line in lines:
         splitted_line = line.split()
         coords.append([float(splitted_line[1]), float(splitted_line[2])])
         heights.append(float(splitted_line[5]))
 
+    # convert coordinates and heights to np array
     coords = np.array(coords)
     heights = np.array(heights)
 
@@ -236,19 +252,35 @@ def read_krdz_file(filename):
     return res
 
 
-def interpolate_coordinates(res, xlim, ylim):
+def interpolate_coordinates(res: Dict, xlim: List, ylim: List) -> (List, List, np.ndarray):
+    """
+    interpolate all data of each data on the height data locations of the first measurement date
 
+    :param res: fugro results dictionary
+    :param xlim: x limit
+    :param ylim: y limit
+    :return:
+    """
+
+    # initialise lists
     dates = []
     coordinate_data = []
     height_data = []
     interpolated_heights = []
 
+    # loop over dates and data
     for date, res_at_t in zip(res["dates"],res["data"]):
+
+        # get only the data within limits at time t
         coordinates_in_range, heights_in_range = filter_data_within_bounds(xlim, ylim, res_at_t)
+
+        # if coordinates are within limitts, add dates, coordinates and height data to list
         if coordinates_in_range.size > 0:
             dates.append(date)
             coordinate_data.append(coordinates_in_range)
             height_data.append(heights_in_range)
+
+            # interpolate height data on coordinates of the first measurement date
             interpolated_height = griddata(coordinates_in_range, heights_in_range, (coordinate_data[0][:,0], coordinate_data[0][:,1]), method='linear')
             interpolated_heights.append(interpolated_height)
 
@@ -258,21 +290,28 @@ def interpolate_coordinates(res, xlim, ylim):
 
 
 def read_trajectory_qc_data(filename):
+    """
+    Reads data from trajectory qc file
+    :param filename:
+    :return:
+    """
     with open(filename, mode='r') as csv_file:
         csv_reader = csv.reader(csv_file)
         rows = []
         for row in csv_reader:
-
             rows.append(row)
         return rows
 
 def get_lat_long_from_qc_data(qc_data):
+    """
+    Gets latitude and longitude coords from qc data
+    :param qc_data:
+    :return:
+    """
 
     lat = []
     long = []
-
     for i, row in enumerate(qc_data):
-
         if i>0:
             lat.append(row[3])
             long.append(row[4])
@@ -281,37 +320,47 @@ def get_lat_long_from_qc_data(qc_data):
 
 
 def convert_lat_long_to_rd(lat, long):
+    """
+    Converts latitude and longitude coordinates to rd coordinates
+    :param lat:
+    :param long:
+    :return:
+    """
 
     crs_wgs = pyproj.Proj(init='epsg:4326')
     crs_bng  = pyproj.Proj(init="epsg:28992")
-
 
     x, y = pyproj.transform(crs_wgs, crs_bng, long, lat)
 
     return x,y
 
-def read_krdz_data_from_csv(filename):
+def read_rila_data_from_csv(filename) -> Dict:
+    """
+    Reads rila data from csv file
+    :param filename: csv file name
+    :return:
+    """
 
-
+    # read all lines from csv file
     with open(filename, mode='r') as csv_file:
         csv_reader = csv.reader(csv_file)
         lines = []
         for line in csv_reader:
-
             lines.append(line)
 
-
+    # initialise results dictionary
     res = {"coordinates": None,
            "height": None}
 
     coords = []
     heights = []
-
+    # adds coordinates and heights to list
     for i, line in enumerate(lines):
         if i>0:
             coords.append([float(line[0]), float(line[1])])
             heights.append(float(line[2]))
 
+    # convert coordinates and heights to nd arrays
     coords = np.array(coords)
     heights = np.array(heights)
 
@@ -332,7 +381,7 @@ if __name__ == '__main__':
 
     filename = r"D:\software_development\ROSE\data\Fugro\Amsterdam-Eindhoven TKI Project\01_Amsterdam_Utrecht\Amsterdam_Utrecht_201811.csv"
 
-    read_krdz_data_from_csv(filename)
+    read_rila_data_from_krdz(filename)
 
     a=1+1
     # file_dir = r"D:\software_development\ROSE\data\Fugro\Amsterdam_Eindhoven\Deltares_AmsterdamEindhovenKRDZ"
