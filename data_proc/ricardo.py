a405fb7d02865b414d05758ec43422b2f9b6ab5c
noordam <aron.noordam@deltares.nl>
2021-05-11 14:27:43 +0200

    corrected and improved speed of reading ricardo data
    Former-commit-id: 6dc46b0bb3a1a5afac3846a33c6b18e9465dd464

--- data_proc/ricardo.py
+++ data_proc/ricardo.py
@@ -1,7 +1,13 @@
 import os
 import pickle
 import json
+
+from typing import List, Dict
+
 from pyproj import Transformer
+import numpy as np
+import matplotlib.pyplot as plt
+
 from rose.utils import signal_proc
 
 
@@ -24,13 +30,14 @@ def convert_lat_long_to_rd(lat, long):
     return x, y
 
 
-def read_inframon(file_names, output_f):
+def read_inframon(file_names: List, output_f: str):
 
     results = {}
     for fi in file_names:
         with open(fi, "r") as f:
             data = json.load(f)
 
+        # initialise dict
         name = os.path.splitext(os.path.split(fi)[1])[0]
         results.update({name: {"time": [],
                                "coordinates": [],
@@ -40,19 +47,61 @@ def read_inframon(file_names, output_f):
                                "segment": [],
                                }})
 
-        for i in range(len(data)):
-            results[name]["time"].append(data[i]['t'])
-            results[name]["coordinates"].append(convert_lat_long_to_rd(data[i]['lat'], data[i]['lon']))
-            results[name]["speed"].append(data[i]['speed'])
-            results[name]["acc_side_1"].append(data[i]['acc_side_1'])
-            results[name]["acc_side_2"].append(data[i]['acc_side_2'])
-            results[name]["segment"].append(data[i]['Segment'])
+        # get lat and lon coordinates
+        lat = np.array([data[i]['lat'] for i in range(len(data))]).astype(float)
+        lon = np.array([data[i]['lon'] for i in range(len(data))]).astype(float)
 
-    results[name]["acc_side_1"] = signal_proc.filter_sig(results[name]["acc_side_1"],
-                                                         settings_filter["FS"], settings_filter["n"], settings_filter["cut-off"]).tolist()
-    results[name]["acc_side_2"] = signal_proc.filter_sig(results[name]["acc_side_2"],
-                                                         settings_filter["FS"], settings_filter["n"], settings_filter["cut-off"]).tolist()
+        # find invalid indices
+        nan_indices_lat = np.argwhere(np.isnan(lat))
+        nan_indices_lon = np.argwhere(np.isnan(lon))
+        nan_indices = np.unique(np.append(nan_indices_lat,nan_indices_lon))
 
+        # mask invalid indices
+        mask = np.ones(lat.shape, bool)
+        mask[nan_indices] = False
+
+        # covert data to nd arrays
+        time = np.array([data[i]['t'] for i in range(len(data))]).astype(float)[mask]
+        coordinates = convert_lat_long_to_rd(lat[mask], lon[mask])
+        speed = np.array([data[i]['speed'] for i in range(len(data))]).astype(float)[mask]
+        acc_side_1 = np.array([data[i]['acc_side_1'] for i in range(len(data))]).astype(float)[mask]
+        acc_side_2 = np.array([data[i]['acc_side_2'] for i in range(len(data))]).astype(float)[mask]
+        segment = np.array([data[i]['Segment'] for i in range(len(data))])[mask]
+
+        # add data to res dictionary
+        results[name]["time"] = time
+        results[name]["coordinates"] = coordinates
+        results[name]["speed"] = speed
+        results[name]["acc_side_1"] = acc_side_1
+        results[name]["acc_side_2"] = acc_side_2
+        results[name]["segment"] = segment
+
+    #     # results[name]["acc_side_1"] = signal_proc.filter_sig(results[name]["acc_side_1"],
+    #     #                                                      settings_filter["FS"], settings_filter["n"], settings_filter["cut-off"]).tolist()
+    #     # results[name]["acc_side_2"] = signal_proc.filter_sig(results[name]["acc_side_2"],
+    #     #                                                      settings_filter["FS"], settings_filter["n"], settings_filter["cut-off"]).tolist()
+    #
+    #     tmp = signal_proc.filter_sig(results[name]["acc_side_1"],
+    #                                                          settings_filter["FS"], settings_filter["n"],
+    #                                                          settings_filter["cut-off"]).tolist()
+    #     tmp2 = signal_proc.filter_sig(results[name]["acc_side_2"],
+    #                                                          settings_filter["FS"], settings_filter["n"],
+    #                                                          settings_filter["cut-off"]).tolist()
+    #
+    #     f1, a1, _, _ = signal_proc.fft_sig(results[name]["acc_side_2"], settings_filter["FS"])
+    #     f2, a2, _, _ = signal_proc.fft_sig(np.array(tmp2), settings_filter["FS"])
+    #
+    #
+    # plt.plot(f1, a1)
+    # plt.plot(f2, a2)
+    # plt.xlim(0,  settings_filter["FS"]/2)
+    # plt.show()
+    #
+    # plt.plot(results[name]["acc_side_2"])
+    # plt.plot(tmp2)
+    # plt.show()
+
+    # write results to pickle
     with open(os.path.join(output_f, "inframon.pickle"), "wb") as f:
         pickle.dump(results, f)
 
