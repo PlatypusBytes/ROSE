909cbe76f015a0dcfb7bfb57fd4fc3608cfcd473
noordam <aron.noordam@deltares.nl>
2021-06-08 18:23:37 +0200

    added comments and docstrings to model_part.py
    Former-commit-id: 2e2221964aa3c64700db4d49b47bc9f74aaa7974

--- rose/model/model_part.py
+++ rose/model/model_part.py
@@ -2,11 +2,14 @@ import numpy as np
 import rose.model.utils as utils
 import logging
 from typing import List
+import abc
 
 
 # todo move this class
 class Material:
     """
+    This class contains the material of the rail.
+
     :Attributes:
 
         - :self.youngs_modulus:  youngs modulus of the material [N/m2]
@@ -34,6 +37,9 @@ class Material:
     def shear_modulus(self):
         """
         Shear modulus of the material [N/m2]
+
+        ..math
+            G = E/(2*(1+\nu))
         :return:
         """
         return self.youngs_modulus / (2 * (1 + self.poisson_ratio))
@@ -42,6 +48,8 @@ class Material:
 # todo move this class
 class Section:
     """
+    This class contains the cross section of the rail.
+
     :Attributes:
 
         - :self.area:                    section area [m^2]
@@ -77,7 +85,7 @@ class ModelPart:
         - :self.nodes:           all nodes within the model part [-]
         - :self.elements:        all elements within the model part [-]
         - :self.total_n_dof:     total number of degree of freedoms within the model part [-]
-"""
+    """
     def __init__(self):
         self.name = ""
         self.nodes = np.array([])
@@ -146,6 +154,7 @@ class ModelPart:
     def initialize(self):
         """
         Inititalises model parts. Calculates total number of degree of freedom and maps model part on each element
+
         :return:
         """
         self.calculate_total_n_dof()
@@ -171,6 +180,20 @@ class ModelPart:
 
 
 class ElementModelPart(ModelPart):
+    """
+    Element model part class. This class is the base for each element model part type.
+    The model part has to consist of the same element types. This class bases from
+    :class:`~rose.model.model_part.ModelPart`.
+    #todo extend for 2d/3d elements.
+
+    :Attributes:
+
+        - :self.aux_stiffness_matrix:           auxiliary local stiffness matrix
+        - :self.aux_damping_matrix:             auxiliary local damping matrix
+        - :self.aux_mass_matrix:                auxiliary local mass matrix
+        - :self.static_force_vector:            local static force vector within the element
+    """
+
     def __init__(self):
         super(ElementModelPart, self).__init__()
         self.aux_stiffness_matrix = None
@@ -185,6 +208,10 @@ class ElementModelPart(ModelPart):
         self.__rotation_matrix = None
 
     def initialize(self):
+        """
+        Initialises model part. Local stiffness, mass and damping matrices are initialised.
+        :return:
+        """
         super().initialize()
         self.set_aux_stiffness_matrix()
         self.set_aux_mass_matrix()
@@ -194,43 +221,115 @@ class ElementModelPart(ModelPart):
 
     @property
     def normal_shape_functions(self):
+        """
+        Shape functions for the local normal axis
+        :return:
+        """
         return self._normal_shape_functions
 
     @property
     def y_shape_functions(self):
+        """
+        Shape functions for the local shear axis
+        :return:
+        """
         return self._y_shape_functions
 
     @property
     def z_rot_shape_functions(self):
+        """
+        Shape functions for the local rotation around the z-axis
+        :return:
+        """
         return self._z_rot_shape_functions
 
     @property
     def rotation_matrix(self):
+        """
+        Rotation matrix which is used to rotate from the local to the global system.
+
+        :return:
+        """
         return self.__rotation_matrix
 
+    @abc.abstractmethod
     def set_rotation_matrix(self, rotation, dim):
+        """
+        Sets the rotation matrix. This function is meant as an abstract method
+
+        :param rotation: rotation of the element
+        :param dim: dimensions of the element
+        :return:
+        """
         pass
 
+    @abc.abstractmethod
     def set_normal_shape_functions(self, x):
+        """
+        Sets the local normal shape functions at distance x. This function is meant as an abstract method
+
+        :param x: local distance from the first node to the desired locatin within the element
+        :return:
+        """
         pass
 
+    @abc.abstractmethod
     def set_y_shape_functions(self, x):
+        """
+        Sets the local shear shape functions at distance x. This function is meant as an abstract method
+
+        :param x: local distance from the first node to the desired locatin within the element
+        :return:
+        """
         pass
 
+    @abc.abstractmethod
     def set_z_rot_shape_functions(self, x):
+        """
+        Sets the rotation around the z-axis shape functions at distance x. This function is meant as an abstract method
+
+        :param x: local distance from the first node to the desired location within the element
+        :return:
+        """
         pass
 
+    @abc.abstractmethod
     def set_aux_stiffness_matrix(self):
+        """
+        Sets the local auxiliary stiffness matrix. This function is meant as an abstract method
+        :return:
+        """
         pass
 
+    @abc.abstractmethod
     def set_aux_damping_matrix(self):
+        """
+        Sets the local auxiliary damping matrix. This function is meant as an abstract method
+        :return:
+        """
         pass
 
+    @abc.abstractmethod
     def set_aux_mass_matrix(self):
+        """
+        Sets the local auxiliary mass matrix. This function is meant as an abstract method
+        :return:
+        """
         pass
 
 
 class RodElementModelPart(ElementModelPart):
+    """
+    Rod element model part class. This class bases from
+    :class:`~rose.model.model_part.ElementModelPart`. A rod element only interacts in normal direction.
+
+    :Attributes:
+
+        - :self.mass:                           mass of the rod element
+        - :self.stiffness:                      stiffness of the rod element
+        - :self.damping :                       of the rod element
+        - :self.length_element:                 length of the rod element
+    """
     def __init__(self):
         super().__init__()
         self.mass = None
@@ -240,13 +339,26 @@ class RodElementModelPart(ElementModelPart):
 
     @property
     def normal_dof(self):
-         return True
+        """
+
+        :return: is normal degree of freedom activated
+        """
+        return True
 
     @property
     def rotation_matrix(self):
+        """
+        Rotation matrix which is used to rotate from the local to the global system.
+
+        :return:
+        """
         return self.__rotation_matrix
 
     def validate_input(self):
+        """
+        Validates input. Checks if rod model part contains a stiffness, elements and nodes
+        :return:
+        """
         if self.stiffness is None:
             logging.error("Stiffness of rod element is not defined")
 
@@ -259,7 +371,9 @@ class RodElementModelPart(ElementModelPart):
     def set_rotation_matrix(self, rotation, dim):
         """
         Sets 2D rotation matrix
-        :param rotation:
+
+        :param rotation: rotation in the global system
+        :param dim:     dimension of the work space
         :return:
         """
         if dim ==2:
@@ -270,6 +384,10 @@ class RodElementModelPart(ElementModelPart):
             self.__rotation_matrix [[2, 5], [2, 5]] = 1
 
     def set_aux_mass_matrix(self):
+        """
+        Sets the local auxiliary mass matrix for the rod element. Only if the rod contains a mass value
+        :return:
+        """
         if self.mass is not None:
             self.aux_mass_matrix = np.zeros((2, 2))
             self.aux_mass_matrix[0, 0] = 2
@@ -279,6 +397,10 @@ class RodElementModelPart(ElementModelPart):
             self.aux_mass_matrix = self.aux_mass_matrix.dot(self.mass/6)
 
     def set_aux_stiffness_matrix(self):
+        """
+        Sets the local auxiliary stiffness matrix for the rod element. Only if the rod contains a stiffness value.
+        :return:
+        """
         if self.stiffness is not None:
             self.aux_stiffness_matrix = np.zeros((2, 2))
             self.aux_stiffness_matrix[0, 0] = self.stiffness
@@ -287,6 +409,10 @@ class RodElementModelPart(ElementModelPart):
             self.aux_stiffness_matrix[1, 1] = self.stiffness
 
     def set_aux_damping_matrix(self):
+        """
+        Sets the local auxiliary damping matrix for the rod element. Only if the rod contains a damping value.
+        :return:
+        """
         if self.damping is not None:
             self.aux_damping_matrix = np.zeros((2, 2))
             self.aux_damping_matrix[0, 0] = self.damping
@@ -295,21 +421,43 @@ class RodElementModelPart(ElementModelPart):
             self.aux_damping_matrix[1, 1] = self.damping
 
     def initialize_shape_functions(self):
+        """
+        Initialises normal shape functions for the rod element.
+        :return:
+        """
         self._normal_shape_functions = np.zeros(2)
 
     def set_normal_shape_functions(self, x):
+        """
+        Sets the local normal shape functions for the rod element at distance x.
+
+        :param x: local distance from the first node to the desired location within the element
+        :return:
+        """
         self._normal_shape_functions[0] = 1 - x / self.length_element
         self._normal_shape_functions[1] = x / self.length_element
 
 
 class TimoshenkoBeamElementModelPart(ElementModelPart):
+    """
+    Timoshenko beam element model part class. This class bases from
+    :class:`~rose.model.model_part.ElementModelPart`. This class can be used as an euler beam, if a timoshenko factor of
+    0 is used.
+
+    :Attributes:
+
+        - :self.material:           material of the timoshenko beam
+        - :self.section:            cross section of the timoshenko beam
+        - :self.length_element:     length of the timoshenko beam element
+        - :self.nodal_ndof:         number of nodal degrees of freedom in the timoshenko beam (3 in a 2D space)
+        - :self.mass:               mass of the timoshenko beam element
+    """
+
     def __init__(self):
         super().__init__()
 
         self.material = Material()
         self.section = Section()
-
-        self.__timoshenko_factor = 0
 
         self.length_element = None
         self.nodal_ndof = 3
@@ -320,29 +468,58 @@ class TimoshenkoBeamElementModelPart(ElementModelPart):
         self._y_shape_functions = np.zeros(4)
         self._z_rot_shape_functions = np.zeros(4)
 
+        self.__timoshenko_factor = 0
+
 
 
     @property
     def normal_dof(self):
+        """
+
+        :return: is normal degree of freedom activated
+        """
         return True
 
     @property
     def y_disp_dof(self):
+        """
+
+        :return: is y degree of freedom activated
+        """
         return True
 
     @property
     def z_rot_dof(self):
+        """
+
+        :return: is rotation around z-axis degree of freedom activated
+        """
         return True
 
     @property
     def timoshenko_factor(self):
+        """
+        Ratio of beam bending to shear stiffness.
+
+        :return:
+        """
         return self.__timoshenko_factor
 
     @property
     def rotation_matrix(self):
+        """
+        Rotation matrix which is used to rotate from the local to the global system.
+
+        :return:
+        """
         return self.__rotation_matrix
 
     def validate_input(self):
+        """
+        Validates Timoshenko beam model part. Validates material, validates cross section. Checks if model part contains
+        nodes and elements.
+        :return:
+        """
         if not isinstance(self.material, Material):
             logging.error("Material not defined")
         else:
@@ -360,9 +537,19 @@ class TimoshenkoBeamElementModelPart(ElementModelPart):
             logging.error("Timoshenko beam model part does not contain nodes")
 
     def calculate_mass(self):
+        """
+        Calculates the mass per meter of the Timoshenko beam from the cross section and the material density.
+
+        :return:
+        """
         self.mass = self.section.area * self.material.density
 
     def calculate_timoshenko_factor(self):
+        """
+        Calculates the ratio of the beam bending to shear stiffness.
+
+        :return:
+        """
         if self.section.shear_factor > 0:
             self.__timoshenko_factor = (
                 12
@@ -378,8 +565,9 @@ class TimoshenkoBeamElementModelPart(ElementModelPart):
 
     def set_rotation_matrix(self, rotation, dim):
         """
-        Sets 2D rotation matrix
-        :param rotation:
+        Sets 2D rotation matrix of the timoshenko beam
+        :param rotation: rotation in the global system
+        :param dim: dimension of the workspace
         :return:
         """
         if dim ==2:
@@ -391,7 +579,7 @@ class TimoshenkoBeamElementModelPart(ElementModelPart):
 
     def __set_translational_aux_mass_matrix(self):
         """
-        timoshenko straight beam auxiliar mass matrix associated with translational inertia
+        Timoshenko straight beam auxiliary mass matrix associated with translational inertia
         :return:
         """
         phi = self.__timoshenko_factor
@@ -441,7 +629,7 @@ class TimoshenkoBeamElementModelPart(ElementModelPart):
 
     def __set_rotational_aux_mass_matrix(self):
         """
-        timoshenko straight beam auxiliar mass matrix associated with rotatory inertia
+        Timoshenko straight beam auxiliary mass matrix associated with rotatory inertia
         :return:
         """
         phi = self.__timoshenko_factor
@@ -474,8 +662,8 @@ class TimoshenkoBeamElementModelPart(ElementModelPart):
 
     def set_aux_mass_matrix(self):
         """
-        timoshenko straight beam auxiliar mass matrix. If timoshenko factor is equal to 0, no rotational part of the
-        mass matrix is added
+        Timoshenko straight beam auxiliar mass matrix. If timoshenko factor is equal to 0, no rotational part of the
+        mass matrix is added. This is equivalent to an euler beam.
         :return:
         """
         self.aux_mass_matrix = self.__set_translational_aux_mass_matrix()
@@ -485,7 +673,7 @@ class TimoshenkoBeamElementModelPart(ElementModelPart):
 
     def set_aux_stiffness_matrix(self):
         """
-        timoshenko straight beam auxiliar stiffness matrix
+        Timoshenko straight beam auxiliary stiffness matrix
         :return:
         """
         phi = self.__timoshenko_factor
@@ -493,8 +681,6 @@ class TimoshenkoBeamElementModelPart(ElementModelPart):
 
         EI = self.material.youngs_modulus * self.section.sec_moment_of_inertia
         constant = EI / ((1 + phi) * l ** 3)
-
-
 
         self.aux_stiffness_matrix = np.zeros((6, 6))
         self.aux_stiffness_matrix[[0, 3], [0, 3]] = (
@@ -519,14 +705,28 @@ class TimoshenkoBeamElementModelPart(ElementModelPart):
         )
 
     def set_aux_damping_matrix(self):
+        """
+        Timoshenko straight beam auxiliary damping matrix
+        :return:
+        """
         self.aux_damping_matrix = np.zeros((6, 6))
 
     def initialize_shape_functions(self):
+        """
+        Initialises timoshenko beam shape functions as numpy arrays.
+        :return:
+        """
         self._normal_shape_functions = np.zeros(2)
         self._y_shape_functions = np.zeros(4)
         self._z_rot_shape_functions = np.zeros(4)
 
     def set_normal_shape_functions(self, x):
+        """
+        Sets the local normal shape functions for the timoshenko beam element at distance x.
+
+        :param x: local distance from the first node to the desired location within the element
+        :return:
+        """
         self._normal_shape_functions[0] = 1 - x / self.length_element
         self._normal_shape_functions[1] = x / self.length_element
 
@@ -536,6 +736,7 @@ class TimoshenkoBeamElementModelPart(ElementModelPart):
 
         |B.S.Gan, An Isogeometric Approach to Beam Structures, Chapter 3
         DOI10.1007/978-3-319-56493-7_3
+
         :param x: local coordinate of the element [m]
         :return:
         """
@@ -575,6 +776,11 @@ class TimoshenkoBeamElementModelPart(ElementModelPart):
         pass
 
     def initialize(self):
+        """
+        Initialises timoshenko beam model part. Calculates the timoshenko factor; calculates the mass of the beam;
+        initialises shape functions; initialises parent class.
+        :return:
+        """
         self.calculate_timoshenko_factor()
         self.calculate_mass()
         self.initialize_shape_functions()
@@ -582,11 +788,19 @@ class TimoshenkoBeamElementModelPart(ElementModelPart):
 
 
 class ConditionModelPart(ModelPart):
+    """
+    Condition model part class. This class is the base for each boundary condition model part type. This class bases
+    from :class:`~rose.model.model_part.ModelPart`.
+    """
     def __init__(self):
         super(ConditionModelPart, self).__init__()
 
 
 class ConstraintModelPart(ConditionModelPart):
+    """
+    Constraint model part class. This class is a model part which indicates rotational or translational constraints.
+    This class bases from :class:`~rose.model.model_part.ConditionModelPart`.
+    """
     def __init__(self, x_disp_dof=False, y_disp_dof=False, z_rot_dof=False):
         super(ConstraintModelPart, self).__init__()
         self.__x_disp_dof = x_disp_dof
@@ -595,20 +809,30 @@ class ConstraintModelPart(ConditionModelPart):
 
     @property
     def x_disp_dof(self):
+        """
+        :return: is degree of freedom  in x direction activated
+        """
         return self.__x_disp_dof
 
     @property
     def y_disp_dof(self):
+        """
+        :return: is degree of freedom  in y direction activated
+        """
         return self.__y_disp_dof
 
     @property
     def z_rot_dof(self):
+        """
+        :return: is degree of freedom  around z axis activated
+        """
         return self.__z_rot_dof
 
-    def set_scalar_condition(self):
-        pass
-
     def set_constraint_condition(self):
+        """
+        Sets the constraint boundary condition on the nodes.
+        :return:
+        """
         for node in self.nodes:
             node.x_disp_dof = self.__x_disp_dof
             node.z_rot_dof = self.__z_rot_dof
