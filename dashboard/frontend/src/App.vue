8debd10d6a261e97fa8d2fdb44fa5badfab1d94d
IoannaMi <ioannamicha@hotmail.com>
2021-10-04 15:03:17 +0200

    Add components for navigation bar
    * Add AppSideBar component
    * Add CollapbisbleCard
    * Add LayerGroup
    * Add TimeSlider
    * Add TrainType
    
    
    Former-commit-id: 5818c97d247722f25d11671dfb908aa918f2a48f

--- dashboard/frontend/src/App.vue
+++ dashboard/frontend/src/App.vue
@@ -1,98 +1,99 @@
 <template>
-  <v-app> 
-    <v-stepper
-      v-model="page"
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+      height="72px"
+      clipped-left
     >
-      <v-stepper-header>
-        <v-stepper-step
-          :complete="page >1"
-          step="1"
-        >
-          Input data
-        </v-stepper-step>
-        <v-divider />
-           
-        <v-stepper-step
-          :complete="page> 2"
-          step="2"
-        >
-          Results
-        </v-stepper-step>
-      </v-stepper-header>
-      
-      <v-app-bar clipped-left />
-      <v-navigation-drawer
-        clipped
-        permanent
-        width="400"
-      />
-      <v-main>
+      <v-toolbar-title>ROSE </v-toolbar-title>
+      <v-spacer />
+      <v-stepper
+        v-model="page"
+        flat
+        width="80%"
+        height="100%"
+      >
+        <v-stepper-header>
+          <v-stepper-step
+            :complete="page >1"
+            step="1"
+          >
+            Input data
+          </v-stepper-step>
+          <v-divider />
+
+          <v-stepper-step
+            :complete="page> 2"
+            step="2"
+          >
+            Results
+          </v-stepper-step>
+        </v-stepper-header>
+      </v-stepper>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-if="page===2"
+      clipped
+      app  
+      permanent
+      width="400"
+    >
+      <app-side-bar />
+    </v-navigation-drawer> 
+    <v-main app>
+      <v-stepper
+        v-model="page"
+        flat
+        class="pa-0"
+      >
         <v-stepper-items>
           <v-stepper-content step="1">
             <input-data-card @go-to-results="onShowResultsPage" />
-          </v-stepper-content> 
-          <v-stepper-content step="2">
-            <!--  <v-container fluid> 
-              <v-row no-gutters pa-0>
-                <v-col cols="2">
-                  <v-card> 
-                    <v-card-title>
-                      Input Data
-                    </v-card-title>
-                  </v-card>
-                </v-col>
-                <v-col>
-                  <v-card elevation="0" height="600">
-                    <v-card-title>
-                      Input Data
-                    </v-card-title>
-                  </v-card>
-                </v-col>
-              </v-row>
-            </v-container> -->
-
-            <mapbox-map :access-token="accessToken" />
+          </v-stepper-content>
+          <v-stepper-content step="2" class="pa-0">
+            <v-card
+              class="pa-0"
+              height="92vh"
+            >
+              <mapbox-map :access-token="accessToken" />
+            </v-card>
           </v-stepper-content>
         </v-stepper-items>
-      </v-main>
-    </v-stepper>
+      </v-stepper>
+    </v-main>
   </v-app>
 </template>
 
 
 <script>
-  
+
   import { MapboxMap } from '@deltares/vue-components'
   import InputDataCard from '~/components/InputDataCard/InputDataCard'
+  import AppSideBar from '~/components/AppSideBar/AppSideBar'
+
+
 
   export default {
     components: {
       MapboxMap,
       InputDataCard,
+      AppSideBar,
+   
     },
     data: () => ({
       accessToken: process.env.VUE_APP_MAPBOX_TOKEN,
-      page: 1,
+      page: 2,
     }),
     methods: {
       onShowResultsPage(event) {
         this.page = event
       },
     },
-
   }
 </script>
 
 <style lang="scss" scoped>
-/* 
-  .v-stepper__header {
-    height: 8vh;
-  } */
-
-  .v-stepper__content {
-    padding: 0px;
-  }
-
   .mapbox-map__title {
     position: absolute;
     z-index: 1;
@@ -107,17 +108,7 @@
     margin: 0;
   }
 
-
   #map {
     z-index: 5;
   }
-
-  .container {
-    height: 800px;
-  }
-
-  ::v-deep .v-application--wrap {
-    min-height: fit-content;
-  }
-  
 </style>
---
+++ dashboard/frontend/src/components/LayerGroup/LayerGroup.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-radio-group
+    v-model="input"
+    class="pa-4"
+    @change="changed"
+  >
+    <v-radio
+      v-for="layer in layers"
+      :key="layer.id"
+      :label="layer.name"
+      :value="layer.id"
+    />
+  </v-radio-group>
+</template>
+
+<script>
+  export default {
+    data() {
+      return { 
+        layers: [
+          {
+            'name': 'Mean',
+            'id': 'mean',
+          }, 
+          { 'name': 'STD',
+            'id': 'std' } ],
+        input: 'mean',
+      }
+    },
+    methods: {
+      changed(id) {
+        console.log('@changed id', id)
+      },
+    },
+  }
+
+</script>
+/* 
+layers on props
+on mounted take props read layers and set default layer to show
+@change load new layer
+
+ */
