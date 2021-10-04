9b1c2ce2ebf82535de8487a966ca6104d1fa84c5
Bruno Zuada Coelho <bruno.zuadacoelho@deltares.nl>
2021-10-04 17:06:19 +0200

    cleaned up the application.py
    Former-commit-id: d60fed7b6aa03252461cf529b541e8ac27116e0a

--- dashboard/application.py
+++ dashboard/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, Response
 import os
 import json
 import numpy as np
@@ -12,8 +12,12 @@ app = Flask(__name__)
 
 # poth for the local calculations
 CALCS_PATH = "../dash_calculations"
+CALCS_JSON = "calculations.json"
 if not os.path.isdir(CALCS_PATH):
     os.makedirs(CALCS_PATH)
+    # create json
+    with open(os.path.join(CALCS_PATH, CALCS_JSON), "w") as f:
+        json.dump({}, f)
 
 
 @app.route("/", methods=["GET"])
@@ -23,49 +27,53 @@ def index():
 
 @app.route("/runner", methods=["POST"])
 def run():
-    import os
-    print(os.getcwd())
 
-    # parse input json from Front End
+    # ToDo: parse input json from Front End
     input_json = "../run_rose/example_rose_input.json"
 
-    # check input json
-    status = check_calculation(input_json)
+    # check input json & runs calculation
+    message = calculation(input_json)
 
-    return str(status)
+    return message
 
 
-def check_calculation(input_json):
+def calculation(input_json):
     r"""
     Reads and validates the input json file
 
     @param input_json: input json file
-    @return: True / False
     """
 
+    # read json file
     with open(input_json, "r") as fi:
         input = json.load(fi)
 
     # validates json input
     status = validate_input.check_json(input)
 
+    # ToDo: if file is not valid renders input not valid
     if not status:
         return render_template("message.html",  message="Input file not valid")
 
-    # check if file exists & has results
-    # hash the file
+    # hash file, check if file exists & has results
     hash = hashing.Hash()
     hash.hash_dict(input)
 
-    # load calculations:
-    with open(os.path.join(CALCS_PATH, "calculations.json"), "r") as f:
+    # load calculations.json
+    with open(os.path.join(CALCS_PATH, CALCS_JSON), "r") as f:
         calcs = json.load(f)
 
+    # if hash exists visualise results
     if hash.hash_value in calcs.keys():
-        # todo load results
-        return render_template("message.html", message="Calculation exists")
+        #ToDo: load results
+        return render_template("message.html", message="Calculation exists. Load results")
 
-    # ToDo: go to run calculation
+    # ToDo: run calculation
+    # runner(input_json)
+    # add hash to calculations.json
+    calcs.update({str(hash.hash_value): "path_to_geojson"})
+    with open(os.path.join(CALCS_PATH, CALCS_JSON), "w") as fo:
+        json.dump(calcs, fo, indent=2)
     return render_template("message.html", message="Calculation will run")
 
 
