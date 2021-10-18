62d2c3bdfd0f3a50b11e55a912bc578ca52ed511
noordam <aron.noordam@deltares.nl>
2021-10-18 13:31:05 +0200

    added test for valid input json file. Also added utils to clean project and wait for sub threads to end
    Former-commit-id: 6e4a90ed7581b999cd2fe7d015e363f0ab9a3871

--- dashboard/tests/test_application.py
+++ dashboard/tests/test_application.py
@@ -1,7 +1,13 @@
+import threading
+
 from dashboard.application import app
+from dashboard.tests.utils import TestUtils
+
+from flask import request
+
+import json
 
 def test_run_empty_file():
-
     with app.test_client() as c:
 
         # test file
@@ -18,13 +24,53 @@ def test_run_empty_file():
         res_dict = rv.get_json()
 
         # set expected results
-        expected_res_dict={'data': {},
-                           'exist': False,
-                           # 'message': "Input file not valid; InfraMon input is not valid",
-                           'message': "Input file not valid",
-                           'running': False,
-                           "valid": False}
+        expected_res_dict = {'data': {},
+                             'exist': False,
+                              # 'message': "Input file not valid; InfraMon input is not valid",
+                             'message': "Input file not valid",
+                             'running': False,
+                             "valid": False}
 
         # assert results
         assert res_dict == expected_res_dict
+
+def test_valid_calculation():
+    from datetime import timedelta
+
+    app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(seconds=5)
+
+    with open("test_data/test_rose_input.json", "r") as file:
+        sos_segment_input = json.load(file)
+
+    # test file
+    data = {'SOS_Segment_Input': sos_segment_input,
+            'Sensar_Input': None,
+            'Rila_Input': None,
+            'InfraMon_Input': None,
+            }
+    with app.test_client() as c:
+        # run runner
+
+        rv = c.post("/runner", json=data)
+
+        # get results
+        res_dict = rv.get_json()
+
+        # set expected results
+        expected_res_dict = {'data': {},
+                             'exist': False,
+                             'message': "Calculation running",
+                             'running': True,
+                             "valid": False}
+
+        # assert dictionary
+        assert res_dict == expected_res_dict
+
+        # wait for all threads to finish
+        TestUtils.wait_for_subthreads()
+
+        # clean calculation project data
+        TestUtils.delete_calculation_data('proj1')
+
+
 
