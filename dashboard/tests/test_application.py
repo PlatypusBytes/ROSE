65cc95e2f3a7ba2343fc8a3d239b7de1f9b31744
noordam <aron.noordam@deltares.nl>
2021-10-19 10:37:56 +0200

    added test for valid ricardo input json file.
    Former-commit-id: c0f93879f7d15bc279747a547b854e0d0a7c0324

--- dashboard/tests/test_application.py
+++ dashboard/tests/test_application.py
@@ -3,11 +3,11 @@ import threading
 from dashboard.application import app
 from dashboard.tests.utils import TestUtils
 
-from flask import request
-
 import json
+app.config["TESTING"] = True
 
 def test_run_empty_file():
+
     with app.test_client() as c:
 
         # test file
@@ -35,9 +35,6 @@ def test_run_empty_file():
         assert res_dict == expected_res_dict
 
 def test_valid_calculation():
-    from datetime import timedelta
-
-    app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(seconds=5)
 
     with open("test_data/test_rose_input.json", "r") as file:
         sos_segment_input = json.load(file)
@@ -72,5 +69,44 @@ def test_valid_calculation():
         # clean calculation project data
         TestUtils.delete_calculation_data('proj1')
 
+
+def test_valid_calculation_ricardo():
+
+    with open("test_data/test_rose_input.json", "r") as file:
+        sos_segment_input = json.load(file)
+
+    with open("test_data/test_ricardo_input.json", "r") as file:
+        ricardo_input = json.load(file)
+
+    # test file
+    data = {'SOS_Segment_Input': sos_segment_input,
+            'Sensar_Input': None,
+            'Rila_Input': None,
+            'InfraMon_Input': ricardo_input,
+            }
+    with app.test_client() as c:
+        # run runner
+
+
+        rv = c.post("/runner", json=data)
+
+        # get results
+        res_dict = rv.get_json()
+
+        # set expected results
+        expected_res_dict = {'data': {},
+                             'exist': False,
+                             'message': "Calculation running",
+                             'running': True,
+                             "valid": False}
+
+        # assert dictionary
+        assert res_dict == expected_res_dict
+
+        # wait for all threads to finish
+        TestUtils.wait_for_subthreads()
+
+        # clean calculation project data
+        TestUtils.delete_calculation_data('proj1')
 
 
