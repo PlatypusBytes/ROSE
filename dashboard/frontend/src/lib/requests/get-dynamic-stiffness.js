c3bc3b6238488e00d6bdb148e76cb3e5b1674362
IoannaMi <ioannamicha@hotmail.com>
2021-11-08 11:01:24 +0100

    Remove unused line of code
    Former-commit-id: 2b0b677881f6220282787ef41e6dfa74e37f71d9

--- dashboard/frontend/src/lib/requests/get-dynamic-stiffness.js
+++ dashboard/frontend/src/lib/requests/get-dynamic-stiffness.js
@@ -6,10 +6,6 @@ export default function(params) {
     method: 'get',
     url: `${ process.env.VUE_APP_API_ENDPOINT }/dynamic_stiffness`,
     params,
-    proxy:{
-      host:'localhost',
-      port:5000,
-    },
   }).then(({ data }) => {
 		return typeof data === 'object' ? data : JSON.parse(data)
 	})
