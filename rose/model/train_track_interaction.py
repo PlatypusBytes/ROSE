d1134da75b0ac477fd76fde8708bd0d7562d8664
noordam <aron.noordam@deltares.nl>
2022-09-05 09:56:58 +0200

    altered precision to check for location of wheels on track
    Former-commit-id: 5f04de161c086fa613d74903b4ed07f9ccec6880

--- rose/model/train_track_interaction.py
+++ rose/model/train_track_interaction.py
@@ -369,8 +369,8 @@ class CoupledTrainTrack(GlobalSystem):
         # get limits wheels
         limits_wheel_distances = np.array([[min(wheel.distances), max(wheel.distances)] for wheel in self.train.wheels])
 
-        # precision is machine float64 precision * n time steps
-        precision = np.finfo(float).eps * len(self.train.wheels[0].distances)
+        # precision is machine float32 epsilon
+        precision = np.finfo(np.float32).eps
 
         # check if train is on track at all times
         if (limits_wheel_distances < limits_track[0] - precision).any() or \
