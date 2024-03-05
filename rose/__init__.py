8b3f32208a592249f1d6a0ab7ab496f09cf9dccd
Bruno Zuada Coelho <brunozuada@gmail.com>
2024-03-05 15:18:46 +0100

    Add __version__.py file and update setup.cfg

--- rose/__init__.py
+++ rose/__init__.py
@@ -1,11 +1,1 @@
-"""
-Rose Library
-"""
-
-__version__ = "0.0.1"
-
-from . import model
-from . import optimisation
-from . import post_processing
-from . import pre_process
-from . import utils
+from .__version__ import __version__
---
+++ rose/__version__.py
@@ -0,0 +1,4 @@
+__title__ = "ROSE"
+__url__ = "https://rose.readthedocs.io"
+__version__ = "0.2"
+__author__ = "ROSE team"
