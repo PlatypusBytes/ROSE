2be628fc2a516a594895ce22f355384de3048645
brunozc <brunozuada@gmail.com>
2025-03-20 12:13:00 +0100

    Enhance random field generation with log-normal option and add corresponding tests

--- rose/utils/random_field.py
+++ rose/utils/random_field.py
@@ -4,7 +4,7 @@ import gstools as gs
 
 
 def create_rf(mean: float, coefficient_variation: float, len_scale: float, angles: float, nodes: np.ndarray,
-              seed: int=14):
+              seed: int=14, log_normal: bool=False) -> np.ndarray:
     """
     Create a 1D random field with a given mean and coefficient of variation.
 
@@ -16,34 +16,63 @@ def create_rf(mean: float, coefficient_variation: float, len_scale: float, angle
     angles (float): angle of the random field
     nodes (np.ndarray): nodes of the random field
     seed (int): seed for the random field
+    log_normal (bool): if True, the random field will be log-normal
 
     Returns
     -------
     np.ndarray: 1D random field
     """
-    sigma = mean * coefficient_variation
-    variance = sigma ** 2
-    model = gs.Gaussian(dim=1, var=variance, len_scale=len_scale, angles=angles)
-    rf = gs.SRF(model, mean=mean, seed=seed)
-    return rf.structured(nodes)
+
+    # Compute parameters for the normal / log-normal field
+    if log_normal:
+        mu_prime = np.log(mean) - 0.5 * np.log(1 + coefficient_variation**2)
+        sigma_prime = np.sqrt(np.log(1 + coefficient_variation**2))
+    else:
+        mu_prime = mean
+        sigma_prime = mean * coefficient_variation
+
+    # Define the Gaussian model with the transformed variance
+    model = gs.Gaussian(dim=1, var=sigma_prime**2, len_scale=len_scale, angles=angles)
+
+    # Generate the random field
+    rf = gs.SRF(model, mean=mu_prime, seed=seed)
+    rf.structured(nodes)
+
+    # If log-normal, exponentiate the field
+    if log_normal:
+        return np.exp(rf.field)
+    else:
+        return rf.field
 
 
 if __name__ == '__main__':
-    mean = 132
-    cov = 0.25
+    mean = 5
+    cov = 0.3
     nodes = np.linspace(0, 10000, 10000) * 0.6
     rfield = create_rf(mean, cov, 1, 0, nodes)
 
-    fig, ax = plt.subplots(2, 1)
-    ax[0].plot(nodes, rfield, 'b-', label="RF")
-    ax[0].plot(nodes, np.ones(len(nodes)) * mean, 'r-', label="mean")
-    ax[0].set_ylim(bottom=0)
-    ax[0].grid()
-    ax[0].legend()
+    rfield_lognormal = create_rf(mean, cov, 1, 0, nodes, log_normal=True)
 
-    ax[1].hist(np.random.normal(mean, mean * cov, 10000), density=True, bins=100, color="b")
-    ax[1].hist(rfield, bins=100, density=True, color='r')
-    ax[1].grid()
-    ax[1].set_ylim(bottom=0)
-    ax[1].set_xlim(left=0)
+    fig, ax = plt.subplots(2, 2)
+    ax[0, 0].plot(nodes, rfield, 'b-', label="RF")
+    ax[0, 0].plot(nodes, np.ones(len(nodes)) * mean, 'r-', label="mean")
+    # ax[0, 0].set_ylim(bottom=0)
+    ax[0, 0].grid()
+    ax[0, 0].legend()
+
+    ax[1, 0].hist(np.random.normal(mean, mean * cov, 10000), density=True, bins=100, color="b")
+    ax[1, 0].hist(rfield, bins=100, density=True, color='r')
+    ax[1, 0].grid()
+    # ax[1, 0].set_ylim(bottom=0)
+    # ax, 0[1].set_xlim(left=0)
+
+    ax[0, 1].plot(nodes, rfield_lognormal, 'b-', label="RF")
+    ax[0, 1].plot(nodes, np.ones(len(nodes)) * mean, 'r-', label="mean")
+    ax[0, 1].grid()
+    ax[0, 1].legend()
+
+    ax[1, 1].hist(np.random.lognormal(np.log(mean) - 0.5 * np.log(1 + cov**2), np.sqrt(np.log(1 + cov**2)), 10000), density=True, bins=100, color="b")
+    ax[1, 1].hist(rfield_lognormal, bins=100, density=True, color='r')
+    ax[1, 1].grid()
+
     plt.show()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,13 +3,22 @@ import os
 import numpy as np
 
 from rose.utils.Hmax import HrsmMax
+from rose.utils.random_field import create_rf
+
 
 TEST_PATH = "tests"
 
 
 class TestUtils:
+    """
+    Test the utility functions in the utils module.
+    """
 
     def test_Hmax(self):
+        """
+        Test the Hmax calculation against the MATLAB implementation.
+        """
+
         with open(os.path.join(TEST_PATH, "./test_data/Hrms.txt"), "r") as fi:
             sig = fi.read().splitlines()
 
@@ -35,3 +44,39 @@ class TestUtils:
         assert all((h.max_fast - h_max_matlab) / h_max_matlab <= tol)
         assert all((h.max_fast_Dx - h_max_dx_matlab) / h_max_dx_matlab <= tol)
 
+
+    def test_normal_random_field_statistics(self):
+        """
+        Test normal random field statistics.
+        """
+        # Setup
+        mean = 5.0
+        cov = 0.3
+        nodes = np.linspace(0, 1000, 10000)
+
+        # Execute
+        rf = create_rf(mean, cov, 1.0, 0.0, nodes, seed=14)
+
+        # Verify
+        assert rf.shape == nodes.shape
+        assert np.isclose(np.mean(rf), mean, rtol=0.1)
+        assert np.isclose(np.std(rf), mean * cov, rtol=0.1)
+
+    def test_lognormal_random_field_statistics(self):
+        """
+        Test log-normal random field statistics.
+        """
+        # Setup
+        mean = 5.0
+        cov = 0.3
+        nodes = np.linspace(0, 1000, 10000)
+
+        # Execute
+        rf = create_rf(mean, cov, 1.0, 0.0, nodes, seed=14, log_normal=True)
+
+        # Verify
+        assert rf.shape == nodes.shape
+        assert np.isclose(np.mean(rf), mean, rtol=0.15)
+        empirical_cov = np.std(rf) / np.mean(rf)
+        assert np.isclose(empirical_cov, cov, rtol=0.15)
+        assert np.all(rf > 0)
